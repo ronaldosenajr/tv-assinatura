cf63d0b859130d8e9b8c87dcd3119d5f39db418f
RonaldoJr <ronaldosenajr@gmail.com>
2025-07-12 20:54:37 -0300

    Refac: to works in api mode (json response)

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,13 +1,16 @@
 class ClientsController < ApplicationController
   # permite acesso nao autenticado mas somente para testes, tem que ser criado um sistema de autenticação
-  allow_unauthenticated_access only: %i[ index show new create update edit destroy ]
+  skip_before_action :verify_authenticity_token
+  allow_unauthenticated_access
   before_action :set_client, only: [ :show, :edit, :update, :destroy ]
 
   def index
     @clients = Client.all
+    render json: @clients
   end
 
   def show
+    render json: @client
   end
 
   def new
@@ -18,11 +21,11 @@ class ClientsController < ApplicationController
     @client = Client.new(client_params)
     if @client.save
       # Se você quiser retornar um JSON, descomente a linha abaixo
-      # render json: @client, status: :created
+      render json: @client, status: :created
       # Pode redirecionar para a página de exibição do cliente
-      redirect_to @client
+      # redirect_to @client
     else
-      render json: @client.errors.full_messages, status: :unprocessable_entity
+      render json: { errors: @client.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
@@ -32,26 +35,29 @@ class ClientsController < ApplicationController
   def update
     if @client.update(client_params)
       # Se você quiser retornar um JSON, descomente a linha abaixo
-      # render json: @client, status: :created
+      render json: @client, status: :created
       # Pode redirecionar para a página de exibição do cliente
-      redirect_to @client
+      # redirect_to @client
     else
-      render json: @client.errors.full_messages, status: :unprocessable_entity
+      render json: { errors: @client.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def destroy
     @client.destroy
     # Se você quiser retornar um JSON, descomente a linha abaixo
-    # render json: @client, status: :created
+    render json: @client, status: :created
     # Pode redirecionar para a página de exibição do cliente
-    redirect_to clients_path
+    # redirect_to clients_path
   end
 
 private
 
   def set_client
-    @client = Client.find(params[:id])
+    @client = Client.find_by(id: params[:id])
+    if @client.nil?
+      render json: { error: "Client not found" }, status: :not_found
+    end
   end
 
   def client_params
