ae7db9e9c1a8959b784caa8e04a149d5d1806553
RonaldoJr <ronaldosenajr@gmail.com>
2025-07-12 19:42:51 -0300

    Fix: adjusting to older age

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,5 @@
 class Client < ApplicationRecord
     validates :name, presence: true
-    validates :age, numericality: { only_integer: true, greater_than: 18 }
+    validates :age, numericality: { only_integer: true, greater_than: 17 }
 
 end
