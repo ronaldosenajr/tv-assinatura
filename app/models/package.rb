adfb7bfe01ed8afd2cc9871709cf8e633ccdb28b
RonaldoJr <ronaldosenajr@gmail.com>
2025-07-12 21:23:03 -0300

    Feat: check if package has additional_service

--- app/models/package.rb
+++ app/models/package.rb
@@ -6,4 +6,13 @@ class Package < ApplicationRecord
   validates :name, presence: true
   validates :plan, presence: true
   validates :value, numericality: true, allow_nil: true
+
+  validate  :must_have_at_least_one_service
+
+  private
+    def must_have_at_least_one_service
+      if additional_services.empty?
+        errors.add(:additional_services, "é preciso ter pelo menos um serviço adicional")
+      end
+    end
 end
